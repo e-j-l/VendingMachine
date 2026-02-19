5075e9d185429d9108451f901da0b395671528e2
a-r-t <alexthimineur@gmail.com>
2026-02-19 09:31:03 -0500

    user can select a snack from the available options

--- VendingMachine.java
+++ VendingMachine.java
@@ -25,7 +25,19 @@ public class VendingMachine {
 
         // Snack inventory: names and prices
         String[] snacks = {"Snickers", "Doritos", "M&Ms", "Cheetos", "Cheez-It"};
-        int[] prices = {1, 3, 2, 3, 2}; 
+        double[] prices = {1.5, 1.25, 1.75, 1.5, 1.10}; 
 
+        System.out.println("Welcome to the Vending Machine!");
+        System.out.println("Here you can choose from a large variety of snacks.");
+        System.out.println("Below are your snack options:");
+        System.out.println();
+
+        for (int i = 0; i < snacks.length; i++) {
+            System.out.println(String.format("%d: %s -- $%.2f", i + 1, snacks[i], prices[i]));
+        }
+
+        System.out.println("Which item would you like to choose (enter associated number)?");
+        int choice = scanner.nextInt();
+        System.out.println(String.format("You chose %s, which costs $%.2f", snacks[choice], prices[choice]));
     }
 }
