dd2358f819d99c46ed9dd8181cb1e4a42e57e031
e-j-l <emmettjleary@gmail.com>
2026-02-21 14:06:54 -0500

    added a transaction feature that allows users to add money and make purchases. I also fixed indexing issue that was occuring when picking a snack.

--- VendingMachine.java
+++ VendingMachine.java
@@ -9,16 +9,6 @@ import java.util.Scanner;
 
 public class VendingMachine {
     
-    static class Snack {
-        String name;
-        int price; //in dollars
-
-        Snack(String name, int price) {
-            this.name = name;
-            this.price = price;
-        }
-    }
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -27,17 +17,34 @@ public class VendingMachine {
         String[] snacks = {"Snickers", "Doritos", "M&Ms", "Cheetos", "Cheez-It"};
         double[] prices = {1.5, 1.25, 1.75, 1.5, 1.10}; 
 
+        // Welsome messages 
         System.out.println("Welcome to the Vending Machine!");
         System.out.println("Here you can choose from a large variety of snacks.");
         System.out.println("Below are your snack options:");
         System.out.println();
 
-        for (int i = 0; i < snacks.length; i++) {
+        // Prints all snack options
+        for (int i = 0; i < snacks.length; i++) { 
             System.out.println(String.format("%d: %s -- $%.2f", i + 1, snacks[i], prices[i]));
         }
 
+        // Asks user to select and item and stores it 
         System.out.println("Which item would you like to choose (enter associated number)?");
-        int choice = scanner.nextInt();
-        System.out.println(String.format("You chose %s, which costs $%.2f", snacks[choice], prices[choice]));
+        int choice = scanner.nextInt() - 1; // was having a indexing error so I subtracted 1 to fix the issue.
+        System.out.println(String.format("You chose %s, which costs $%.2f", snacks[choice], prices[choice]
+        ));
+
+        // Asks user to add money and stores info
+        System.out.println("Please add an ammount of money to machine to continue.");
+        double money = scanner.nextDouble();
+
+        // checks to make sure enough money was added and either fails or completes purchase and prints change
+        if(money < prices[choice]) {
+            System.out.println("Not enough money. Please try again."); // was curious if there was a way to send the user back a step
+
+        } else {
+            double change = money - prices[choice];
+            System.out.println(String.format("Purchase complete. \nChange: %.2f", change));
+        }
     }
 }
